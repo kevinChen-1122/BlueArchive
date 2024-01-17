80f78c092fe7891c6e78219f37f67fe0f9c835f8
Kevin <fortis013@gmail.com>
2024-01-17 17:37:12 +0800

    [feature] create attack type class

---
+++ js/attack_type.js
@@ -0,0 +1,37 @@
+/**
+ * 攻擊類型
+ */
+class Attack_type {
+    static types = {
+        /**
+         * 爆炸
+         */
+        EXPLOSION: {
+            ID: 1,
+            /**
+             * 名稱
+             */
+            name: "爆炸"
+        },
+        /**
+         * 貫穿
+         */
+        PENETRATION: {
+            ID: 2,
+            /**
+             * 名稱
+             */
+            name: "貫穿"
+        },
+        /**
+         * 神秘
+         */
+        MYSTIC: {
+            ID: 3,
+            /**
+             * 名稱
+             */
+            name: "神秘"
+        }
+    }
+}
