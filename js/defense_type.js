59e4038b353bb8f3c9fda4f2e0e99fe441276518
Kevin <fortis013@gmail.com>
2024-01-17 17:44:11 +0800

    [feature] create defense type class

---
+++ js/defense_type.js
@@ -0,0 +1,37 @@
+/**
+ * 防禦類型
+ */
+class Defense_type {
+    static types = {
+        /**
+         * 輕裝甲
+         */
+        LIGHT_ARMOR: {
+            ID: 1,
+            /**
+             * 名稱
+             */
+            name: "輕裝甲"
+        },
+        /**
+         * 重裝甲
+         */
+        HEAVY_ARMOR: {
+            ID: 2,
+            /**
+             * 名稱
+             */
+            name: "重裝甲"
+        },
+        /**
+         * 特殊裝甲
+         */
+        SPECIAL_ARMOR: {
+            ID: 3,
+            /**
+             * 名稱
+             */
+            name: "特殊裝甲"
+        }
+    }
+}
