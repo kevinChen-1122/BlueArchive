f1d7fa31960b19766d7e167ce44a7edb82e3e5c1
Kevin <fortis013@gmail.com>
2024-01-24 17:18:04 +0800

    [refactor] refactor class

--- js/role.js
+++ js/role.js
@@ -2,76 +2,99 @@
  * 戰術作用
  */
 class Role {
-    static TYPES = {
-        STRIKER: {
-            ID: 1,
-            /**
-             * 名稱
-             */
-            name: "STRIKER",
-            /**
-             * 探索屬性
-             */
-            exploration_attributes: new Exploration_attributes({
-                "strength": 0,
-                "size": 0,
-                "intuition": 0,
-                "willpower": 0,
-                "mystery": 0,
-                "luck": 0
-            }),
-            /**
-             * 戰鬥屬性
-             */
-            battle_attributes: new Battle_attributes({
-                "health_point": 20,
-                "attack": 0,
-                "defense": 0,
-                "heal": 0,
-                "dexterity": 1,
-                "accurate": 0,
-                "agility": 0
-            }),
-            /**
-             * 獎勵點
-             */
-            bonus_point: 10
+    /**
+     *
+     * @param {string} name 名稱
+     * @param {number} bonus_point 獎勵點
+     * @param {object} exploration_attributes 探索屬性
+     * @param {object} battle_attributes 戰鬥屬性
+     */
+    constructor(name = '', bonus_point = 0, exploration_attributes = {}, battle_attributes = {}) {
+        /**
+         * 名稱
+         * @type {string}
+         * @private
+         */
+        this._name = name;
+        /**
+         * 獎勵點
+         * @type {number}
+         * @private
+         */
+        this._bonus_point = bonus_point;
+        /**
+         * 探索屬性
+         * @type {Exploration_attributes}
+         * @private
+         */
+        this._exploration_attributes = new Exploration_attributes(exploration_attributes);
+        /**
+         * 戰鬥屬性
+         * @type {Battle_attributes}
+         * @private
+         */
+        this._battle_attributes = new Battle_attributes(battle_attributes);
+    }
+    static LIST = [
+        "STRIKER",
+        "SPECIAL"
+    ];
+    static config = {
+        striker:{
+            name:"STRIKER",
+            bonus_point: 10,
+            exploration_attributes:{
+                strength: 0,
+                size: 0,
+                intuition: 0,
+                willpower: 0,
+                mystery: 0,
+                luck: 0
+            },
+            battle_attributes:{
+                health_point: 20,
+                attack: 0,
+                defense: 0,
+                heal: 0,
+                dexterity: 1,
+                accurate: 0,
+                agility: 0
+            }
         },
-        SPECIAL: {
-            ID: 2,
-            /**
-             * 名稱
-             */
-            name: "SPECIAL",
-            /**
-             * 探索屬性
-             */
-            exploration_attributes: new Exploration_attributes({
-                "strength": 0,
-                "size": 0,
-                "intuition": 0,
-                "willpower": 0,
-                "mystery": 0,
-                "luck": 0
-            }),
-            /**
-             * 戰鬥屬性
-             */
-            battle_attributes: new Battle_attributes({
-                "health_point": 0,
-                "attack": 1,
-                "defense": 0,
-                "heal": 0,
-                "dexterity": 0,
-                "accurate": 1,
-                "agility": 0
-            }),
-            /**
-             * 獎勵點
-             */
-            bonus_point: 10
+        special:{
+            name:"SPECIAL",
+            bonus_point: 10,
+            exploration_attributes:{
+                strength: 0,
+                size: 0,
+                intuition: 0,
+                willpower: 0,
+                mystery: 0,
+                luck: 0
+            },
+            battle_attributes:{
+                health_point: 0,
+                attack: 1,
+                defense: 0,
+                heal: 0,
+                dexterity: 0,
+                accurate: 1,
+                agility: 0
+            }
         }
     };
+    static STRIKER = new Role(
+        Role.config.striker.name,
+        Role.config.striker.bonus_point,
+        Role.config.striker.exploration_attributes,
+        Role.config.striker.battle_attributes
+    );
+    static SPECIAL = new Role(
+        Role.config.special.name,
+        Role.config.special.bonus_point,
+        Role.config.special.exploration_attributes,
+        Role.config.special.battle_attributes
+    );
 
     /**
      * 轉換表格資料
@@ -79,14 +102,15 @@ class Role {
      */
     generate_table_data() {
         let data = [];
-        Object.keys(Role.TYPES).forEach((role_name) => {
-            const role = Role.TYPES[role_name];
-            data.push({
-                "名稱": role.name,
-                "探索屬性": new Exploration_attributes().generate_table_data(role.exploration_attributes),
-                "戰鬥屬性": new Battle_attributes().generate_table_data(role.battle_attributes),
-                "獎勵點": role.bonus_point
-            });
+        Object.values(Role.LIST).forEach((role_name) => {
+            if (Role[role_name] ?? null) {
+                data.push({
+                    "名稱": Role[role_name]._name,
+                    "探索屬性": new Exploration_attributes().generate_table_data(Role[role_name]._exploration_attributes),
+                    "戰鬥屬性": new Battle_attributes().generate_table_data(Role[role_name]._battle_attributes),
+                    "獎勵點": Role[role_name]._bonus_point
+                });
+            }
         });
         return data;
     }
@@ -106,12 +130,13 @@ class Role {
         default_option.selected = true;
         select_element.appendChild(default_option)
 
-        Object.keys(Role.TYPES).forEach((role_name) => {
-            const role_type = Role.TYPES[role_name];
-            const new_option = document.createElement('option')
-            new_option.textContent = role_type.name;
-            new_option.dataset.id = role_name;
-            select_element.appendChild(new_option);
+        Object.values(Role.LIST).forEach((role_name) => {
+            if (Role[role_name] ?? null) {
+                const new_option = document.createElement('option')
+                new_option.textContent = Role[role_name]._name;
+                new_option.dataset.id = role_name;
+                select_element.appendChild(new_option);
+            }
         });
         return select_element;
     }
